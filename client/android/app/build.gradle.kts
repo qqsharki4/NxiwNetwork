6c5ea671560f013124ecd1c68ef8f2d16d5c5a0b
qqsharki4 <qqsharki4@gmail.com>
2026-05-28 21:22:46 +0200

    chore(android): bump dev11

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -7,8 +7,8 @@ val releaseKeystoreProperties = Properties().apply {
         releaseKeystorePropertiesFile.inputStream().use(::load)
     }
 }
-val appVersionCode = 10
-val appVersionName = "1.1.0-dev.10"
+val appVersionCode = 11
+val appVersionName = "1.1.0-dev.11"
 
 fun releaseSigningValue(envName: String, propertyName: String): String? {
     return System.getenv(envName) ?: releaseKeystoreProperties.getProperty(propertyName)
